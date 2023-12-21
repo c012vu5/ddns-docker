85ce3ef103afc5eef19e96ad55619d0bb30af17e
c012vu5 <lynx.mondlicht@gmail.com>
2023-12-21 14:39:37 +0900

    minorfix:change tab to whitespace

--- ddns.go
+++ ddns.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+    "fmt"
+    "math/rand"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 )
 
 var (
-	ACC  = ""
-	PASS = ""
+    ACC  = ""
+    PASS = ""
 )
 
 func main() {
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGTERM)
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, syscall.SIGTERM)
 
-	go func() {
-		sig := <-sigChan
-		fmt.Printf("Received signal: %v\n", sig)
+    go func() {
+        sig := <-sigChan
+        fmt.Printf("Received signal: %v\n", sig)
 
-		os.Exit(0)
-	}()
+        os.Exit(0)
+    }()
 
-	fmt.Println("Global IP notifier started")
+    fmt.Println("Global IP notifier started")
 
-	pulseLoop()
+    pulseLoop()
 }
 
 func pulseLoop() {
-	pulse()
-	for {
-		time.Sleep(time.Duration(getRandomWaitSec()) * time.Second)
-		pulse()
-	}
+    pulse()
+    for {
+        time.Sleep(time.Duration(getRandomWaitSec()) * time.Second)
+        pulse()
+    }
 }
 
 func pulse() {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s:%s@ipv4.mydns.jp/login.html", ACC, PASS), nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
+    client := &http.Client{}
+    req, err := http.NewRequest("GET", fmt.Sprintf("https://%s:%s@ipv4.mydns.jp/login.html", ACC, PASS), nil)
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        return
+    }
 
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Error sending request:", err)
+        return
+    }
+    defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusOK {
-		fmt.Println("ADDRESS update success.")
-	} else {
-		fmt.Println("ADDRESS update failure.")
-	}
+    if resp.StatusCode == http.StatusOK {
+        fmt.Println("ADDRESS update success.")
+    } else {
+        fmt.Println("ADDRESS update failure.")
+    }
 }
 
 func getRandomWaitSec() int {
-	waitSec := rand.Intn(601) + 600
-	return waitSec
+    waitSec := rand.Intn(601) + 600
+    return waitSec
 }
